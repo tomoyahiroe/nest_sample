a2eb717b9d370c389089f0f5906302c6db962eeb
tomoyahiroe <tomoya4creative@gmail.com>
2022-07-23 16:49:44 +0900

    feat: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormConfig from '../ormconfig';
+//import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(ormConfig)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
